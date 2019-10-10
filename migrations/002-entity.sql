a87bd1f3cfd3d6e42d0ab22c3adb80e471897684
ibuckner <ibuckner@work>
2019-10-10 15:07:51 +0100

    Update word lists

--- migrations/002-entity.sql
+++ migrations/002-entity.sql
@@ -4,9 +4,9 @@ INSERT INTO "Entity"
 ( "name", "color", "domain", "type", "reg_ex" ) 
 VALUES 
 ('AddressLine', '#008040', 'Location', 'Regular expression', '\b(?:\d+[a-z]*)\s(?:[''\.\-\w]*)\s(?:approach|avenue|bank|close|cl|crescent|croft|court|drive|east|end|field|heights|ga?r?de?ns|gate|grange|green|grove|hill|lane|ln|meadow|mews|north|parade|park|path|place|pl|rise|road|rd|row|square|south|street|st|terrace|view|walk|way|west|wood|yard)\b'),
-('DateLong', '#00ff80', 'Time', 'Regular expression', '\b(?:[0123]?\d(?:st|nd|rd|th)?(?:[\s\-\.]{0,3}))?(?:jan|janu?a?ry|feb|febr?ua?ry|mar|march|apr|april|may|jun|june|jul|july|aug|august|sept?|september|oct|october|nov|november|dec|december)(?:(?:[\s\-\.?]{0,3})?(?:\d\d|\d\d\d\d))?\b'),
+('DateLong', '#00ff80', 'Time', 'Regular expression', '\b(?:[0123]?\d(?:st|nd|rd|th)?)(?:[\s\-\.]{0,3}|\sof\s)?(?:jan|janu?a?ry|feb|febr?ua?ry|mar|march|apr|april|may|jun|june|jul|july|aug|august|sept?|september|oct|october|nov|november|dec|december)(?:(?:[\s\-\.]{0,3})(?:\d\d|\d\d\d\d))\b'),
+('DayMonth', '#00ff80', 'Time', 'Regular expression', '\b(?:[0123]?\d(?:st|nd|rd|th)?)(?:[\s\-\.]{0,3}|\sof\s)(?:jan|janu?a?ry|feb|febr?ua?ry|mar|march|apr|april|may|jun|june|jul|july|aug|august|sept?|september|oct|october|nov|november|dec|december)\b'),
 ('DateShort', '#00ff90', 'Time', 'Regular expression', '\b[0123]?\d[\s\-\.\/]{1,3}[0123]?\d[\s\-\.\/]{1,3}\d{2,4}|\d{2,4}[\s\-\.\/]{1,3}[0123]?\d[\s\-\.\/]{1,3}[0123]?\d\b'),
-('DateWD', '#00ffcc', 'Time', 'Regular expression', '\b(?:mon|tues?|wed|wednes|thurs?|fri|sat|satur|sun)(?:day)?\b'),
 ('Email', '#ff8000', 'Contact', 'Regular expression', '\b(?:[\w+\-\_\.]+)@(?:[\w+\-\_\.]+)\b'),
 ('Firstname', '#2bbaff', 'Person', 'Word list', NULL),
 ('NHSNumber', '#0040cc', 'NHSNumber', 'Regular expression', '[34679]\d\d\s?\d\d\d\s?\d\s?\d\s?\d\s?\d'),
