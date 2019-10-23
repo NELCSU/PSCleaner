d852e487a46bc97ced40c17d7835461b84324a98
ibuckner <ibuckner@work>
2019-10-23 14:56:40 +0100

    Fix to stablise file queuing

--- static/scripts/index-esm.js
+++ static/scripts/index-esm.js
@@ -9,80 +9,90 @@ const progressBar = one("#stProcessFiles");
 let importFiles = 0;
 let processingFiles = 0;
 let exportFiles = 0;
-let started = false;
-let processing = false;
-
-/**
- * Update the file counts in each badge
- */
-function updateUI() {
-  importBadge.textContent = importFiles;
-  processingBadge.textContent = processingFiles;
-  exportBadge.textContent = exportFiles;
-  runButton.hidden = started || (!started && processing) ? true : importFiles === 0; 
-  runButton.textContent = started ? "Cancel" : "Start processing";
-  progressBar.hidden = !started && !processing;  
-  progressBar.value = exportFiles;
-}
+let running = false;
 
 function setUpProgressBar() {
+  progressBar.value = 0;
   progressBar.max = importFiles + processingFiles + exportFiles;
+  progressBar.hidden = false;
 }
 
-ipc.on("stop-import", () => {
-  started = false;
-  ipc.send("get-file-counts");
-});
+function teardownProgressBar() {
+  progressBar.max = 0;
+  progressBar.hidden = true;
+}
 
-ipc.on("stop-processing", () => {
-  processing = false;
-  updateUI();
-})
-
-ipc.on("imported", () => {
-  updateUI();
-  processing = true;
-  ipc.send("start-processing");  
-});
-
-ipc.on("processed", () => {
-  processing = false;
-  updateUI();
-  ipc.send("start-import");
-});
-
-ipc.on("export-file-count", (e, data) => {
-  exportFiles = data;
-  updateUI();
-});
-
-ipc.on("processing-file-count", (e, data) => {
-  processingFiles = data;
-  updateUI();
-});
-
-ipc.on("import-file-count", (e, data) => {
-  importFiles = data;
-  updateUI();
-});
-
-runButton.addEventListener("click", () => {
-  setUpProgressBar();
-  started = true;
-  updateUI();
-  if (started) {
-    ipc.send("start-import");
+function toggleRun() {
+  running = !running;
+  if (running && (importFiles > 0 || processingFiles > 0)) {
+    runButton.textContent = "Stop";
+    setUpProgressBar();
+    if (processingFiles > 0) {
+      ipc.send("start-processing");
+    } else {
+      ipc.send("start-import");
+    }
+  } else {
+    runButton.textContent = "Start";
+    teardownProgressBar();
   }
-});
+}
 
-ipc.send("import-file-count");
-ipc.send("processing-file-count");
-ipc.send("export-file-count");
+function startImport() {
+  if (processingFiles === 0) {
+    if (importFiles === 0) {
+      toggleRun();
+    } else {
+      ipc.send("start-import");
+    }
+  } else if (processingFiles > 0) {
+    setTimeout(startImport, 500);
+  } else {
+    toggleRun();
+  }
+}
 
-// opens explorer view for each folder via badge click
+runButton.addEventListener("click", toggleRun);
+
+//*** opens explorer view for each folder via badge click
 importBadge.addEventListener("click", () => ipc.send("get-import-folder"));
 processingBadge.addEventListener("click", () => ipc.send("get-processing-folder"));
 exportBadge.addEventListener("click", () => ipc.send("get-export-folder"));
-ipc.on("import-folder", (e, path) => shell.openItem(path));
-ipc.on("processing-folder", (e, path) => shell.openItem(path));
-ipc.on("export-folder", (e, path) => shell.openItem(path));
+ipc.on("import-folder", (_, path) => shell.openItem(path));
+ipc.on("processing-folder", (_, path) => shell.openItem(path));
+ipc.on("export-folder", (_, path) => shell.openItem(path));
+
+//*** initialisation of badge counts
+ipc.send("import-file-count");
+ipc.on("import-file-count", (_, data) => {
+  importFiles = data;
+  importBadge.textContent = importFiles;
+});
+ipc.send("processing-file-count");
+ipc.on("processing-file-count", (_, data) => {
+  processingFiles = data;
+  processingBadge.textContent = processingFiles;
+  if (running && processingFiles > 0) {
+    ipc.send("start-processing");
+  }
+});
+ipc.send("export-file-count");
+ipc.on("export-file-count", (_, data) => {
+  exportFiles = data;
+  exportBadge.textContent = exportFiles;
+  if (running) {
+    progressBar.value = exportFiles;
+    startImport();
+  }
+});
+
+//*** Error handling
+ipc.on("import-folder-error", (_, err) => {
+  console.log(`An error occurred with the import folder ${err}`);
+});
+ipc.on("processing-folder-error", (_, err) => {
+  console.log(`An error occurred with the processing folder ${err}`);
+});
+ipc.on("export-folder-error", (_, err) => {
+  console.log(`An error occurred with the export folder ${err}`);
+});
