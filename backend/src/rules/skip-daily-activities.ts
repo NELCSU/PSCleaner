7d70c89ba0331cebbf350bf0c8c9773e073902ca
ibuckner <ibuckner@work>
2021-01-14 10:01:47 +0000

    v0.9.7

--- backend/src/rules/skip-daily-activities.ts
+++ backend/src/rules/skip-daily-activities.ts
@@ -3,25 +3,33 @@ const a: RegExp[] = [
   /\bbali\sbreeze\b/gmi,
   /\bball\sbearing\b/gmi,
   /\bariel\s(?:liquitab|tablets?|washing)\b/gmi,
+  /\bastro\sturf\b/gmi,
+  // B
+  /\bbaby\swalker\b/gmi,
   /\bbali\sbreeze\b/gmi,
   /\bbatterie\b/gmi,
   /\bbean\sbag\b/gmi,
   /\bbent\s(?:down|inwards|over)\b/gmi,
   /\bblue\stack\b/gmi,
-  /\b(?:bunk|divan)\sbed\b/gmi,
+  /\b(?:bunk|divan|off\sthe|out\sof)\sbed\b/gmi,
+  // C
   /\bcandle\swax\b/gmi,
   /\bswallow[a-z]*\schalk\b/gmi,
+  /\bchain\ssaw\b/gmi,
   /\bcheddar\scheese\b/gmi,
   /\bcotton\sbud\b/gmi,
   /\bclimbing\sframe\b/gmi,
   /\bcut\sthrough\b/gmi,
   /\bcuticura\shand\b/gmi,
+  // D
   /\bdining\sroom\b/gmi,
   /\bdog\scage\b/gmi,
   /\bdog\sbite\b/gmi,
+  // E
   /\beau\sde\scologne\b/gmi,
   /\bear\sring\b/gmi,
   /\beating\smcdonalds\b/gmi,
+  // F
   /\bfalls?\s(?:down|from|off|on|over)\b/gmi,
   /\bfell\s(?:backwards?|down|forward|from|off|on|over)\b/gmi,
   /\bfirst\saider\b/gmi,
@@ -29,30 +37,39 @@ const a: RegExp[] = [
   /\bfrench\sfries\b/gmi,
   /\bfriends\shouse\b/gmi,
   /\b(?:cut|hurt)\s(?:hand|neck|shoulder)\b/gmi,
+  // G
   /\bgas\spipe\b/gmi,
   /\bglass\s(?:cut|table)\b/gmi,
+  // H
   /\bhair\sdryer\b/gmi,
   /\bhard\sedge\b/gmi,
   /\bhead\sinj(?:ury)?\b/gmi,
   /\b(?:bleach|(?:hot\s)?water)\sbottle\b/gmi,
   /\bbottle\sof\s(?:bleach|(?:hot\s)?water)\b/gmi,
   /\bhover\sboard\b/gmi,
+  /\bhula\shoop\b/gmi,
+  // J
   /\b(?:canning|mason)\sjar\b/gmi,
   /\b(?:ji?u)?[\s-]?jitsu\b/gmi,
   /\blower\sleg\b/gmi,
   /\b(?:car|lift)\sjack\b/gmi,
+  // M
   /\bmartial\sarts?\b/gmi,
   /\bmoses\sbasket\b/gmi,
   /\b(?:muddy|playing|rugby)\sfields?\b/gmi,
+  // O
   /\bon\s(?:shelf|table)\b/gmi,
+  // P
   /\bpiece\sof\swood\b/gmi,
   /\bpiggy\sback\b/gmi,
   /\bping\spong\b/gmi,
   /\b(?:penny|pound)\scoin\b/gmi,
   /\bplay(?:ed|ing|s)?\s(?:frisby|rugby)\b/gmi,
   /\bplaydoh\b/gmi,
+  // R
   /\b(?:nations\s)?rugby\s(?:league|union)\b/gmi,
   /\brocking\shorse\b/gmi,
+  // S
   /\bscarf\spin\b/gmi,
   /\bskate\s?park\b/gmi,
   /\bslipped\sfoot\b/gmi,
@@ -60,6 +77,8 @@ const a: RegExp[] = [
   /\bstair\sgate\b/gmi,
   /\b(?:stanley|swiss\sarmy)\sknife\b/gmi,
   /\bstung\sby\b/gmi,
+  // T
+  /\btable\stennis\b/gmi,
   /\btaekwondo\b/gmi,
   /\btennis\srackett?\b/gmi,
   /\bthe\sbath\b/gmi,
@@ -67,11 +86,14 @@ const a: RegExp[] = [
   /\btoliet\b/gmi,
   /\btrampol(?:ine)?\b/gmi,
   /\btrod\son\b/gmi,
+  // V
   /\bvenetian\s(?:blind|window)s?\b/gmi,
+  // W
   /\bwendy\shouse\b/gmi,
   /\bwhite\sspirit\b/gmi,
   /\bwindow\ssill\b/gmi,
   /\bwooden\sfloor\b/gmi,
+  // Z
   /\bzimmer\sframe\b/gmi
 ];
 const DailyActivitiesRegEx: Set<RegExp> = new Set();
